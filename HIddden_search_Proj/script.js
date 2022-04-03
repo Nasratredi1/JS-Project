ed119abe5b59ef5906531d3d8b7a2e2bfb6a979c
Nasratredi <nasratredip@gmail.com>
2022-04-03 15:52:09 +0500

    Hidden_Search_proj

---
+++ HIddden_search_Proj/script.js
@@ -0,0 +1,23 @@
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+
+btn.addEventListener('click' , () => {
+    search.classList.toggle('active')
+    input.focus()
+
+})
+
+
+/*
+var myElement = document.getElementById("myElementID");
+myElement.classList.add("style1 style2");
+myElement.classList.remove("style1 style2");
+
+// .classList method returns an array of the classes attached to the element it is called with.
+document.getElementById("myDIV").classList
+// It returns
+// [ 'className1', 'className2', ... ]
+
+*/
